029d37e1ba07e8a175b4785958023c3dde69172c
Calderon, Raymond <rcalder7@CougarNet.UH.EDU>
2026-01-26 19:27:14 -0600

    added new test file OutageServiceTests.cs

---
+++ ReactApp1.Server.UnitTests/OutageServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Options;
+using ReactApp1.Server.Dtos;
+using ReactApp1.Server.Infrastructure.Http;
+using RichardSzalay.MockHttp;
+using System.Net;
+
+namespace ReactApp1.Server.UnitTests
+{
+    public class OutageServiceTests
+    {
+    }
+}
